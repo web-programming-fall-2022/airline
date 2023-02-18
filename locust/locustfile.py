3acec87f5b2c31c1559c5bf37bd4f41f056c1118
Pouya Esmaili <poesmaili@gmail.com>
2023-02-18 21:10:47 +0330

    Update locust tests

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,13 +1,23 @@
-import time
 from locust import HttpUser, task, between
 
-class QuickstartUser(HttpUser):
+
+class AirlineUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
     def origin_destination(self):
-        self.client.get('/api/v1/ticket/origin-dest')
+        self.client.get('/api/v1/ticket/origin-dest?limit=20')
 
     @task
     def available_offers(self):
-        self.client.get('/api/v1/ticket/available-offers')
+        self.client.get('/api/v1/ticket/available-offers?limit=100')
+
+    @task
+    def login_getinfo(self):
+        auth_token = self.client.post('/api/v1/auth/login', json={
+            'email': 'pouya@gmail.com',
+            'password': '123456789',
+        }).json()['authToken']
+        self.client.post('/api/v1/auth/userinfo', json={
+            'auth_token': auth_token
+        })
